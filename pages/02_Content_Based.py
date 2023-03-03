74080ea779de7e077836fe4820c8db07d7cf4fb9
ds-mrtq <126768458+ds-mrtq@users.noreply.github.com>
2023-03-04 00:11:21 +0700

    Add files via upload

--- pages/02_Content_Based.py
+++ pages/02_Content_Based.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 from wordcloud import WordCloud
 import random
+import datetime
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import linear_kernel, cosine_similarity
 from underthesea import word_tokenize, pos_tag, sent_tokenize
@@ -12,115 +13,94 @@ from gensim import corpora, models, similarities
 import jieba
 import re
 import pickle
+from distutils import errors
+from distutils.log import error
+import altair as alt
+from itertools import cycle
+from st_aggrid import GridOptionsBuilder, AgGrid, GridUpdateMode, DataReturnMode, ColumnsAutoSizeMode, JsCode
+from st_clickable_images import clickable_images
 
-from st_aggrid import AgGrid, GridUpdateMode, JsCode
-from st_aggrid.grid_options_builder import GridOptionsBuilder
-import glob
 import streamlit as st
+
 
 # Read data
 data = pd.read_csv('data/ProductRaw.csv')
-data.head()
+# st.write(data.head())
 
+recomend_dict = pd.read_csv('data/CB_new.csv')
+# st.write(recomend_dict.head())
 # Functions
-# Define a function to display product details when a product is clicked
-def display_product_details(product):
-    st.write("## Product Details")
-    st.write(f"**Name:** {product['name']}")
-    st.write(f"**Description:** {product['description']}")
-    st.write(f"**List Price:** {product['list_price']}")
-    st.write(f"**Brand:** {product['brand']}")
-    st.write(f"**Group:** {product['group']}")
+
     
 # GUI
 
 # st.markdown("Contend Based")
 st.sidebar.markdown('# Content Based')
 
-st.subheader("Product Catalog")
+# st.subheader("Product Catalog")
 
 
-# Display a random selection of 6 products in a grid
-random_products = data.sample(n=6)
-random_products.shape[0]
+# Define a search input where the user can type in their query
+search_input = st.text_input('Search for an product')
 
-# n_cols = 3
-# n_rows = 1 + random_products.shape[0] // int(n_cols)
-# # 'n_rows:'
-# # n_rows
-# rows = [st.container() for _ in range(n_rows)]
-# # 'rows:'
-# # rows
-# cols_per_row = [r.columns(n_cols) for r in rows]
-# # 'cols_per_row'
-# # cols_per_row
-# cols = [column for row in cols_per_row for column in row]
-# # 'cols'
-# # cols
-# count = 0
-# for i, product in random_products.iterrows():
-#     # i
-#     # product
-#     cols[count].image(product['image'], width=150, caption=product['name'], 
-#                 use_column_width=True, output_format="PNG")
-#     cols[count].write(f"**Giá:** {product['list_price']}")
-#     count = count + 1
+# Filter the items based on the user's query
+filtered_items = [item for item in data['name'] if search_input.lower() in item.lower()]
+
+# Show the filtered items in a list
+if filtered_items:
+    selected_item = st.selectbox('Select an item', filtered_items[:5])
+    selected_product_id = data[data['name'] == selected_item]['item_id'].values
+    st.write(f"You selected: {selected_product_id} {selected_item}")
+else:
+    st.write('No product found.')
 
 
-# # When a product is clicked, open a new browser tab to display more details
-# if st.button("Click here to select a product"):
-#     selected_product = st.selectbox("Select a product", random_products["name"])
-#     product_info = random_products[random_products["name"] == selected_product].iloc[0]
-#     url = product_info["url"]
-#     js = f"window.open('{url}')"  # JavaScript to open a new tab
-#     html = '<img src="{}" style="max-width: 200px">'.format(product_info["image"])
-#     html += f"<h2>{product_info['name']}</h2>"
-#     html += f"<p>{product_info['description']}</p>"
-#     html += f"<p>List price: {product_info['list_price']}</p>"
-#     html += f"<p>Brand: {product_info['brand']}</p>"
-#     html += f"<p>Group: {product_info['group']}</p>"
-#     st.write("You have selected this product:")
-#     st.write(html, unsafe_allow_html=True)
-#     st.write(
-#         f'<script type="text/javascript">{js}</script>',
-#         unsafe_allow_html=True,
-#     )
+# st.write(type(selected_product_id))
+
+st.markdown("""### Your recommendation products:""")
+
+recomend_items = recomend_dict[recomend_dict['product_id'].isin(selected_product_id)][['product_id', 'rcmd_product_id', 'score']]
+# st.write(recomend_items)
 
 
-# ########
-render_image = JsCode('''
-                      
-    function renderImage(params){
-    // Create a new image element
-        var img = new Image();
-        
-        img.src = params.value;
-        
-        img.width = 35;
-        img.height = 35;
-        
-        return img;
-        
-    }             
-                      ''')
+# print (pd.merge(df1, df2, left_on='id', right_on='id1', how='left').drop('id1', axis=1))
+recomend_result = pd.merge(recomend_items, data, left_on='rcmd_product_id', right_on='item_id', how='left').head(6)
+# recomend_result = data[data['item_id'].isin(recomend_items['rcmd_product_id'])]
+# st.write(recomend_result)
 
-# build gridoptions object
 
-# Build GridOptions object
-options_builder = GridOptionsBuilder.from_dataframe(random_products)
-options_builder.configure_column('image', cellRenderer = render_image)
-options_builder.configure_selection(selection_mode="single", use_checkbox=True)
-grid_options = options_builder.build()
+n_cols = 3
+n_rows = 1 + recomend_result.shape[0] // int(n_cols)
+# 'n_rows:'
+# n_rows
+rows = [st.container() for _ in range(n_rows)]
+# 'rows:'
+# rows
+cols_per_row = [r.columns(n_cols) for r in rows]
+# 'cols_per_row'
+# cols_per_row
+cols = [column for row in cols_per_row for column in row]
+# 'cols'
+# cols
+count = 0
 
-# Create AgGrid component
-grid = AgGrid(random_products, 
-                gridOptions = grid_options,
-                allow_unsafe_jscode=True,
-                height=200, width=500, theme='streamlit')
+for i, product in recomend_result.iterrows():
+    # i
+    # product
+    image_url = product['image']
+    target_url = product['url']
+    product_name = product['name']
+    price = product['list_price']
+    
+    cols[count].image(product['image']
+                      , width=150
+                    #   , caption=f'[{product_name}]({target_url})'
+                    , caption=product['name']
+                      , use_column_width=True
+                      , output_format="PNG")
+    cols[count].write(f"***Rating:*** {product['rating']}")
+    cols[count].write(f"**Giá:** {product['list_price']}")
+    
 
-sel_row = grid["selected_rows"]
-if sel_row:
-    col1, col2 = st.columns(2)
-    st.info(sel_row[0]['description'])
-    col1.image(sel_row[0]['image'],caption = sel_row[0]['name'])
-    col2.subheader("Rating: " + str(sel_row[0]['rating']))
+    count = count + 1
+
