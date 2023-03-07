ca4cc7e506c52c99c6bed07cce8a9e9ed52c0372
ds-mrtq <126768458+ds-mrtq@users.noreply.github.com>
2023-03-07 22:38:16 +0700

    Add files via upload

--- pages/03_Collaborative_Filtering.py
+++ pages/03_Collaborative_Filtering.py
@@ -19,69 +19,7 @@ from st_clickable_images import clickable_images
 
 import streamlit as st
 
-
-# Read data
-data = pd.read_csv('data/Review.csv')
-# st.write(data.head())
-
-recommend_dict = pd.read_parquet('data/collaborative_filtering_all_users_recs.parqet')
-# recommend_dict
-
-products = pd.read_csv('data/ProductRaw.csv')
-
-# st.markdown("Collaborative Filtering")
-st.sidebar.markdown('# Collaborative Filtering')
-
-# Define a search input where the user can type in their query
-search_input = st.text_input('Search for an customer id')
-
-# Filter the items based on the user's query
-filtered_items = [item for item in recommend_dict['customer_id'] if str(search_input).lower() in str(item).lower()]
-
-# Show the filtered items in a list
-if filtered_items:
-    selected_item = st.selectbox('Select a customer', filtered_items[:5])
-    # selected_customer_id = recommend_dict[recommend_dict['customer_id'] == selected_item]['customer_id'].head(1)
-    st.write(f"You selected: {selected_item}")
-else:
-    st.write('No customer found.')
-
-st.markdown("""### Your recommendation products:""")
-# recommend_items = recommend_dict[recommend_dict['customer_id'] == selected_item]['recommendations']
-# recommend_items.iloc[0]
-
-# # Initialize an empty dataframe with columns name
-# recommend_items_df = pd.DataFrame(columns=['customer_id', 'product_id', 'rating'])
-# # st.write(len(recommend_items.iloc[0]))
-
-# # Add values using for loop 
-# i = 0
-# for item in recommend_items.iloc[0]:
-#     # st.write(i)
-#     # st.write(item['product_id'])
-#     recommend_items_df.loc[i] = [selected_item, item['product_id'], item['rating']]
-#     i = i + 1
-# recommend_items_df
-
-recommend_items = recommend_dict.loc[recommend_dict['customer_id'] == selected_item, 'recommendations']
-recommend_items_lst = recommend_items.iloc[0]
-# recommend_items_lst
-
-# Create a list of tuples with required data 
-rcm_item_tuples_list = [(selected_item, item.get('product_id'), item.get('rating')) for item in recommend_items_lst]
-# rcm_item_tuples_list
-
-# Create dataframe directly from the list of tuples
-recommend_items_df = pd.DataFrame(rcm_item_tuples_list, columns=['customer_id', 'product_id', 'rating'])
-# recommend_items_df
-
-# get products list
-products_df = pd.read_csv('data/ProductRaw.csv')
-
-recommend_result = pd.merge(recommend_items_df, products_df, left_on='product_id', right_on='item_id', how='left')
-recommend_result.sort_values(by=['rating_x'], ascending=False, inplace=True)
-# recomend_result
-
+# Functions here
 
 def show_items(recommend_result):
     n_cols = 3
@@ -119,5 +57,76 @@ def show_items(recommend_result):
 
         count = count + 1
 
-show_items(recommend_result)
+def collaborative_filtering(show_items, recommend_dict, selected_item):
+    st.markdown("""### Your recommendation products:""")
+# recommend_items = recommend_dict[recommend_dict['customer_id'] == selected_item]['recommendations']
+# recommend_items.iloc[0]
+
+# # Initialize an empty dataframe with columns name
+# recommend_items_df = pd.DataFrame(columns=['customer_id', 'product_id', 'rating'])
+# # st.write(len(recommend_items.iloc[0]))
+
+# # Add values using for loop 
+# i = 0
+# for item in recommend_items.iloc[0]:
+#     # st.write(i)
+#     # st.write(item['product_id'])
+#     recommend_items_df.loc[i] = [selected_item, item['product_id'], item['rating']]
+#     i = i + 1
+# recommend_items_df
+
+    recommend_items = recommend_dict.loc[recommend_dict['customer_id'] == selected_item, 'recommendations']
+    recommend_items_lst = recommend_items.iloc[0]
+# recommend_items_lst
+
+# Create a list of tuples with required data 
+    rcm_item_tuples_list = [(selected_item, item.get('product_id'), item.get('rating')) for item in recommend_items_lst]
+# rcm_item_tuples_list
+
+# Create dataframe directly from the list of tuples
+    recommend_items_df = pd.DataFrame(rcm_item_tuples_list, columns=['customer_id', 'product_id', 'rating'])
+# recommend_items_df
+
+# get products list
+    products_df = pd.read_csv('data/ProductRaw.csv')
+
+    recommend_result = pd.merge(recommend_items_df, products_df, left_on='product_id', right_on='item_id', how='left')
+    recommend_result.sort_values(by=['rating_x'], ascending=False, inplace=True)
+# recomend_result
+
+
+
+    show_items(recommend_result)
+
+# Read data
+data = pd.read_csv('data/Review.csv')
+# st.write(data.head())
+
+recommend_dict = pd.read_parquet('data/collaborative_filtering_all_users_recs.parqet')
+# recommend_dict
+
+products = pd.read_csv('data/ProductRaw.csv')
+
+# st.markdown("Collaborative Filtering")
+st.sidebar.markdown('# Collaborative Filtering')
+
+# Define a search input where the user can type in their query
+search_input = st.text_input('Search for an customer id')
+
+# Filter the items based on the user's query
+filtered_items = [item for item in recommend_dict['customer_id'] if str(search_input).lower() in str(item).lower()]
+
+# Show the filtered items in a list
+if filtered_items:
+    selected_item = st.selectbox('Select a customer', filtered_items[:5])
+    # selected_customer_id = recommend_dict[recommend_dict['customer_id'] == selected_item]['customer_id'].head(1)
+    st.write(f"You selected: {selected_item}")
+    collaborative_filtering(show_items, recommend_dict, selected_item)
+else:
+    selected_item = st.selectbox('Select a customer', filtered_items[:5])
+    st.write('No customer found.')
+
+
+
+
 
